7b52e970e756f9d9774afbaf1486bd0dd4dad45c
front-end-captain <18810940932@163.com>
2021-03-27 15:55:54 +0800

    feat: supported SEO tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from "react";
 import { NavLink } from "react-router-dom";
+import Helmet from "react-helmet";
 
 import "./App.css";
 
 const App: FunctionComponent = (props) => {
   return (
     <div className="app-wrapper">
+      <Helmet>
+        <meta name="description" content="this is description" />
+        <meta name="description" content="this is another description" />
+      </Helmet>
+
       <NavLink to="/index" activeClassName="activity" exact>
         Index
       </NavLink>
