ba6f1732a75a5494b7fa8239d55c17304cea74c5
front-end-captain <18810940932@163.com>
2021-03-30 17:56:59 +0800

    feat: update global.d.ts(remove module.hot types)

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -2,11 +2,5 @@ interface Window {
   __USE_SSR__?: boolean;
   __INITIAL_DATA__?: any;
 }
-interface NodeModule {
-  hot?: Hot;
-}
-interface Hot {
-  accept(path?: string): void;
-}
 
 declare const __IS_BROWSER__: boolean | undefined;
